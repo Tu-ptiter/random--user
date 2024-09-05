752ecef247d6078c42e8a372ed92422a6bc05ee4
tdv <tuduongvan2305@gmail.com>
2024-09-05 14:23:56 +0700

    save

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null);  
+  const [loading, setLoading] = useState(true);  
+
+  useEffect(() => {
+  
+    axios.get('https://randomuser.me/api/')
+      .then(response => {
+        setUser(response.data.results[0]);
+        setLoading(false);  
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setLoading(false);  
+      });
+  }, []);  
+
+  if (loading) {
+    return ;  
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user ? (
+        <div>
+          <header>
+            <p>RANDOM USER GENERATOR</p>
+          </header>
+          <div class="card">
+            <img src={user.picture.large} alt="User Avatar" class="img" />
+            <p class ="first">Hi, My name is</p>
+            <p class ="profile">{user.name.first} {user.name.last}</p>
+            <p>Email: {user.email}</p>
+            <p>Location: {user.location.city}, {user.location.country}</p>
+          </div>   
+        </div>
+      ) : (
+        <p>No user data available</p>
+      )}
     </div>
   );
 }
